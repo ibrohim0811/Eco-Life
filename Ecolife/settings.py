7ba76d32b0ff0b1382393b2a58171eb51f3b8f3b
Ibrohim <ia1888980@gmail.com>
2026-03-17 22:12:15 +0500

    postgres variables and settings

--- Ecolife/settings.py
+++ Ecolife/settings.py
@@ -12,7 +12,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
+
+# os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
