1cd6c9307fa1575be2b951124e0b861bfad35bc3
Ibrohim <ia1888980@gmail.com>
2026-03-19 17:17:52 +0500

    debug

--- Ecolife/settings.py
+++ Ecolife/settings.py
@@ -12,8 +12,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
+DEBUG = True
+# os.getenv('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = [host.strip() for host in os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")]
